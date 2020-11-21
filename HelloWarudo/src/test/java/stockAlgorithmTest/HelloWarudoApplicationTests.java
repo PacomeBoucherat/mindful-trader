950017f9beae4841a70fc49113c382ff34945a6e
inf101Hylm <44261952+inf101Hylm@users.noreply.github.com>
2020-11-21 11:18:29 +0000

    Added the spring interface and code for printing "panda" on a web page of adress  http://localhost:8080/

---
+++ HelloWarudo/src/test/java/stockAlgorithmTest/HelloWarudoApplicationTests.java
@@ -0,0 +1,13 @@
+package stockAlgorithmTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWarudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

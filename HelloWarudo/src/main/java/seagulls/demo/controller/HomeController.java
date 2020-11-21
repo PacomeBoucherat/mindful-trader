a1dec55a68c4486013648090e78bb3dd54e40a61
user <user@DESKTOP-7SIB75L>
2020-11-21 11:55:57 +0000

    -

--- HelloWarudo/src/main/java/seagulls/demo/controller/HomeController.java
+++ HelloWarudo/src/main/java/seagulls/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-// By typing 1 on the console "hello" will run, otherwise "message"
+ // By typing 1 on the console "hello" will run, otherwise "message"
 	Scanner console = new Scanner(System.in);
 	Integer i = console.nextInt();
 	if (i==1) {

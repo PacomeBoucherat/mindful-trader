0ec28f99e5f25cf47de1fa937544cdb1cfb90b3c
inf101Hylm <44261952+inf101Hylm@users.noreply.github.com>
2020-11-21 09:58:52 +0000

    Test of commit using the web based test 'hello world'

---
+++ HelloWarudo/src/main/java/seagulls/demo/HelloWarudoApplication.java
@@ -0,0 +1,13 @@
+package seagulls.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWarudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWarudoApplication.class, args);
+	}
+
+}

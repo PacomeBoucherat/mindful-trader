3906321b5ed01b2b270cfd880a13cb50bf87568c
user <user@DESKTOP-7SIB75L>
2020-11-21 11:48:16 +0000

    I modified a bit the controller file so to check if it works with Java code

--- HelloWarudo/src/main/java/seagulls/demo/controller/HomeController.java
+++ HelloWarudo/src/main/java/seagulls/demo/controller/HomeController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
+ // By typing 1 on the console "hello" will run, otherwise "message"
+	Scanner console = new Scanner(System.in);
+	Integer i = console.nextInt();
+	if (i==1) {
     public String hello(){
         return "hello";
     }
+	}
+	else {
+		return "message";
+	}
 
     @GetMapping("/message")
     public String message(Model model) {

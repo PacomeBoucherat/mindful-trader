1e60c8b061313c9c55e9909157b2952189a89926
Pacome <44261952+PacomeBoucherat@users.noreply.github.com>
2020-11-21 12:14:43 +0000

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this is a random thing 
 @SpringBootApplication
 public class DemoApplication {
 

1657724486aa661fe7628e24c75fe4b516d16778
user <user@DESKTOP-7SIB75L>
2020-11-22 11:51:21 +0000

    readData() function is implemented. We need to remove the first line and know that first element is the date, so we do not need it.

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.ui.Model;
@@ -8,18 +14,65 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	//static List<List<String>> f = new ArrayList<List<String>>();
+	List<List<String>> readData() {
+		// TODO Auto-generated method stub
+		//String csvFile = "/Users/user/Downloads/IXIC.csv";
+		String csvFile = "/Users/user/git/mindful-trader/demo/src/main/java/com/example/demo/controller/test_data.csv";
+		BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        List<List<String>> f = new ArrayList<List<String>>();
+        
+        
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] file = line.split(cvsSplitBy);
+
+                List<String> a = new ArrayList<String>();
+                for (int i=0;i<5;i++) {
+                	a.add(file[i]);
+                }
+                f.add(a);
+            }
+            System.out.println(f);
+        } /*catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/ catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return f;
+	}
 
     @GetMapping("/")
     public String hello(){
-    	// By typing 1 on the console "hello" will run, otherwise "message"
-    	Scanner console = new Scanner(System.in);
-    	Integer i = console.nextInt();
-    	if (i==1) {
+    	/*List<List<String>> f = new ArrayList<List<String>>();
+    	f = readData();
+    	if ((int)Double.parseDouble(f.get(1).get(1))==8529) {
+    	
         return "hello";
     	}
     	else {
+    		//String d = "Date";
+    		System.out.println(f.get(1).get(1));//.toString().getClass().getName());
     		return "message";
     	}
+    	//System.out.println((int)Double.parseDouble(f.get(1).get(1)));
+    	//return "hello";
+    }*/
+    	return "hello";
     }
 
     @GetMapping("/message")

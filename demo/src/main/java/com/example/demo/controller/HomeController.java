8675c82348e3ee0f498c027f6b56f51820cb49a6
user <user@DESKTOP-7SIB75L>
2020-11-23 21:18:34 +0000

    A modified readData function so it now returns a double [][] type with at most 1 000 000 entries, each entry representing [double, double, double, double] or in our case [Open, High, Low, Close]!

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -12,34 +12,53 @@ import java.util.Scanner;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
 public class HomeController {
 	//static List<List<String>> f = new ArrayList<List<String>>();
-	List<List<String>> readData() {
+	double [][] readData() {
 		// TODO Auto-generated method stub
 		//String csvFile = "/Users/user/Downloads/IXIC.csv";
 		String csvFile = "/Users/user/git/mindful-trader/demo/src/main/java/com/example/demo/controller/test_data.csv";
 		BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-        List<List<String>> f = new ArrayList<List<String>>();
+        double [][] f = new double [1000000][5];// = new ArrayList<List<Double>>();
         
         
         try {
-
+        	Integer t = 0;
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
                 String[] file = line.split(cvsSplitBy);
 
-                List<String> a = new ArrayList<String>();
+                String [] a = new String [5];// = new ArrayList<String>();
+                
                 for (int i=0;i<5;i++) {
-                	a.add(file[i]);
+                	if (i!=0){
+                		a[i-1] = file[i];
+                	//a.add((file[i]));
+                	}
                 }
-                f.add(a);
+                if (t!=0){
+                	double [] b = new double [5];// = new ArrayList<Double>();
+                	for (int i=0;i<4;i++) {
+                		b[i] = Double.parseDouble(a[i]);
+                	//b.add(Double.parseDouble(a.get(i)));
+                	}
+                f[t-1] = b;
+                }
+                t+=1;
             }
-            System.out.println(f);
+            /*for(int i =0;i<f.size();i++) {
+            	for(int j=1;j<f.get(0).size();j++) {
+            		f.get(i).get(j) = Double.parseDouble(f.get(i).get(j));
+            	}
+            }*/
+            //System.out.println(f);
         } /*catch (FileNotFoundException e) {
             e.printStackTrace();
         }*/ catch (IOException e) {
@@ -53,27 +72,28 @@ public class HomeController {
                 }
             }
         }
+        
         return f;
 	}
 
     @GetMapping("/")
     public String hello(){
-    	/*List<List<String>> f = new ArrayList<List<String>>();
+    	double [][] f = new double [1000000][5];// = new ArrayList<List<Double>>();
     	f = readData();
-    	if ((int)Double.parseDouble(f.get(1).get(1))==8529) {
+    	if ((int)(f[0][0])==8529) {
     	
         return "hello";
     	}
     	else {
     		//String d = "Date";
-    		System.out.println(f.get(1).get(1));//.toString().getClass().getName());
+    		System.out.println(f[0][0]);//.toString().getClass().getName());
     		return "message";
     	}
     	//System.out.println((int)Double.parseDouble(f.get(1).get(1)));
     	//return "hello";
-    }*/
-    	return "hello";
     }
+    	/*return "hello";
+    }*/
 
     @GetMapping("/message")
     public String message(Model model) {

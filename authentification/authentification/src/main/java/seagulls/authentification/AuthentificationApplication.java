62ae3030e8f5cf2d83568396e2f554b772272879
Pacome <44261952+PacomeBoucherat@users.noreply.github.com>
2020-11-28 15:11:52 +0000

    Login management
    The folder contains a spring framework that creates a web page asking for username and password to see a message. For now the username is 'user' and the password is 'password', can be changed in the files.

---
+++ authentification/authentification/src/main/java/seagulls/authentification/AuthentificationApplication.java
@@ -0,0 +1,13 @@
+package seagulls.authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationApplication.class, args);
+	}
+
+}
---
+++ authentification/authentification/src/test/java/seagulls/authentification/AuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package seagulls.authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
